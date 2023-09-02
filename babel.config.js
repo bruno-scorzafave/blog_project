0d546765600c5a0152680017704583ff05075d91
BrunoScorzafave <bruno.scorzafave@aluno.ufabc.edu.br>
2023-09-02 17:19:38 -0300

    Initial commit

---
+++ babel.config.js
@@ -0,0 +1,7 @@
+module.exports = function (api) {
+    api.cache(true);
+
+    return {
+      plugins: ['macros', '@babel/plugin-proposal-private-property-in-object'],
+    }
+}
---
+++ src/App.js
@@ -0,0 +1,13 @@
+import User from './Components/User/User';
+
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <User />
+    </div>
+  );
+}
+
+export default App;
