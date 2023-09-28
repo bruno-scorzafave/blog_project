c74f0cfa925cf1da75ba600e12234da03f26f70f
Bruno Scorzafave <brunoscorza@hotmail.com>
2023-09-28 13:24:12 -0300

    Create login page and add Bootstrap5

--- server/main.py
+++ server/main.py
@@ -1,14 +1,26 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
+from flask_wtf import FlaskForm
+from wtforms import StringField, validators
+
+
+class LoginForm(FlaskForm):
+    email = StringField('Email', [validators.Length(min=5)])
+    password = StringField('Password')
+
 
 app = Flask(__name__)
+app.secret_key = "222222"
 
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@app.route("/", methods=['GET', 'POST'])
+def index():
+    login_form = LoginForm()
+    if login_form.validate_on_submit():
+        return redirect('/sucess')
+    return render_template("index.html", form=login_form)
 
 
-@app.route("/<name>")
+@app.route("/ababa/<name>")
 def hello(name):
     return render_template("index.html", name=name)
 
