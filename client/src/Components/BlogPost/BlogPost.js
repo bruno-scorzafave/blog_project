fb6d8f84f483cab37a2d6ebb695e4bc197bbb21d
Bruno Scorzafave <bruno.scorzafave@aluno.ufabc.edu.br>
2024-01-03 15:10:05 -0300

    Add formated values

--- client/src/Components/BlogPost/BlogPost.js
+++ client/src/Components/BlogPost/BlogPost.js
@@ -7,23 +7,23 @@ import PostIcons from '../PostIcons/PostIcons';
 import './BlogPost.scss';
 
 function BlogPost({post}) {
-  return (
-    <div className='BlogPost container bg-white pt-5'>
-        <div className="row blog-item px-3 pb-5">
-            <div className="col-md-5">
-                <img className="img-fluid mb-4 mb-md-0" src="img/blog-1.jpg" alt="" />
-            </div>
-            <div className="col-md-7">
-                <h3 className="px-md-3 mb-2 py-2 bg-white font-weight-bold">{post.title}</h3>
-                <PostIcons tag={'small'}/>
-                <p>
-                    {post.description}
-                </p>
-                <Link className="btn btn-link p-0 text-primary" href={"/post/" + post.title}>Read More <FontAwesomeIcon icon={solid('angle-right')} className='text-primary'/></Link>
+    return (
+        <div className='BlogPost container bg-white pt-5'>
+            <div className="row blog-item px-3 pb-5">
+                <div className="col-md-5">
+                    <img className="img-fluid mb-4 mb-md-0" src="img/blog-1.jpg" alt="" />
+                </div>
+                <div className="col-md-7">
+                    <h3 className="px-md-3 mb-2 py-2 bg-white font-weight-bold">{post.title}</h3>
+                    <PostIcons tag={'small'} created_at={post.created_at} slug={post.slug}/>
+                    <p>
+                        {post.description}
+                    </p>
+                    <Link className="btn btn-link p-0 text-primary" href={"/post/" + post.title}>Read More <FontAwesomeIcon icon={solid('angle-right')} className='text-primary'/></Link>
+                </div>
             </div>
         </div>
-    </div>
-  );
+    );
 }
 
 export default BlogPost;
--- client/src/Components/PostIcons/PostIcons.js
+++ client/src/Components/PostIcons/PostIcons.js
@@ -1,13 +1,21 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { solid } from '@fortawesome/fontawesome-svg-core/import.macro'
 
-function PostIcons({tag}) {
+function PostIcons({tag, created_at, slug}) {
     const CustomTag = `${tag}`;
+    const formattedDate = created_at.slice(0,10);
+    const words = slug.split(' ');
+
+    for (let i = 0; i < words.length; i++) {
+        words[i] = words[i][0].toUpperCase() + words[i].substr(1);
+    }
+    const formattedSlug = words.join(" ");
+    
     return (
         <div className="d-flex">
-            <CustomTag className="me-3 mb-3 text-muted"><FontAwesomeIcon icon={solid('calendar-days')} /> 01-Jan-2045</CustomTag>
-            <CustomTag className="me-3 mb-3 text-muted"><FontAwesomeIcon icon={solid('folder')} /> Web Design</CustomTag>
-            <CustomTag className="me-3 mb-3 text-muted"><FontAwesomeIcon icon={solid('comments')} /> 15 Comments</CustomTag>
+            <CustomTag className="me-3 mb-3 text-muted"><FontAwesomeIcon icon={solid('calendar-days')} /> {formattedDate}</CustomTag>
+            <CustomTag className="me-3 mb-3 text-muted"><FontAwesomeIcon icon={solid('folder')} /> {formattedSlug}</CustomTag>
+            <CustomTag className="me-3 mb-3 text-muted"><FontAwesomeIcon icon={solid('comments')} /> 0 Comments</CustomTag>
         </div>
     );
 }
